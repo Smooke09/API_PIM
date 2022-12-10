8a878e85da1f6fc2d5883c8500b97f469ee5a044
Smooke09 <pedrolucas1014zz@gmail.com>
2022-12-10 12:46:19 -0300

    chore: vercel deployy

--- src/Config/server.ts
+++ src/Config/server.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import cors from "cors";
+const path = require("path");
 import options from "./cors";
 import { routes } from "../routes/index";
 import errorHandlerMiddleware from "../middlewares/errorHandlerMiddleware";
@@ -14,13 +15,25 @@ server.use(express.json());
 server.use(cors(options));
 server.use(express.urlencoded({ extended: false }));
 
+const ROOT_FOLDER = path.join(__dirname, "..");
+app.use(express.static(path.join(ROOT_FOLDER, "public")));
+
 //End poitns
 
 // server.use("/api", authTokenMiddleware, routes);
 server.use("/api", routes);
 
+const optionss = {
+  customCssUrl: "/swagger-ui.css",
+  customSiteTitle: "API AjudaJA swagger",
+};
+
 //Swagger
-server.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+server.use(
+  "/docs",
+  swaggerUi.serve,
+  swaggerUi.setup(swaggerDocument, optionss)
+);
 
 //Middleware error
 server.use(errorHandlerMiddleware);
